6de7c02092a39dcc185f56415b24b368611cef0a
Karim Idrisov <karim.idrisow2016@gmail.com>
2022-05-23 21:10:34 +1000

    back commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
-import { Button, Htag, Tag } from "../components";
+import { useState } from 'react';
+import { Button, Htag, Rating, Tag } from '../components';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
+    const [rating, setRating] = useState<number>(4);
 
-  return (
-    <>
-			<Htag tag='h1'>Текст</Htag>
-      <Button appearance='primary' arrow="right">Кнопка</Button>
-			<Button appearance='ghost' arrow="down">Кнопка</Button>
-      <Tag size='small'>test</Tag>
-      <Tag color='red'>test</Tag>
-      <Tag color='primary'>test</Tag>
-      <Tag color='green'>test</Tag>
-		</>
-  );
+    return (
+        <>
+            <Htag tag="h1">Текст</Htag>
+            <Button appearance="primary" arrow="right">
+                Кнопка
+            </Button>
+            <Button appearance="ghost" arrow="down">
+                Кнопка
+            </Button>
+            <Tag size="small">test</Tag>
+            <Tag color="red">test</Tag>
+            <Tag color="primary">test</Tag>
+            <Tag color="green">test</Tag>
+            <Rating isEditable rating={rating} setRating={setRating} />
+        </>
+    );
 }
+
+export default withLayout(Home);
