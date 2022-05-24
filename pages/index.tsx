49ffa172f193739c0d399fe023391776ec6dc4c2
Karim Idrisov <karim.idrisow2016@gmail.com>
2022-05-24 19:40:58 +1000

    backup commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
 import { Button, Htag, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
     const [rating, setRating] = useState<number>(4);
 
     return (
@@ -19,8 +22,33 @@ function Home(): JSX.Element {
             <Tag color="primary">test</Tag>
             <Tag color="green">test</Tag>
             <Rating isEditable rating={rating} setRating={setRating} />
+            <ul>
+                {menu.map(m => (
+                    <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+                ))}
+            </ul>
         </>
     );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem[]>(
+        process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+        { firstCategory }
+    );
+
+    return {
+        props: {
+            menu,
+            firstCategory,
+        },
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
