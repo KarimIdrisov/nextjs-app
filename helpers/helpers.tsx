cefa41969e9c4fdaa0f2bb69b3c4c4e1e9dcc838
Karim Idrisov <karim.idrisow2016@gmail.com>
2022-05-25 19:54:42 +1000

    backup

---
+++ helpers/helpers.tsx
@@ -0,0 +1,33 @@
+import { TopLevelCategory } from './../interfaces/page.interface';
+import { IFirstLevelMenuItem } from './../interfaces/menu.interface';
+import CoursesIcon from '../layout/Menu/icons/courses.svg';
+import ServicesIcon from '../layout/Menu/icons/services.svg';
+import BooksIcon from '../layout/Menu/icons/books.svg';
+import ProductsIcon from '../layout/Menu/icons/products.svg';
+
+export const firstLevelMenu: IFirstLevelMenuItem[] = [
+    {
+        route: 'courses',
+        name: 'Курсы',
+        icon: <CoursesIcon />,
+        id: TopLevelCategory.Courses,
+    },
+    {
+        route: 'services',
+        name: 'Сервисы',
+        icon: <ServicesIcon />,
+        id: TopLevelCategory.Services,
+    },
+    {
+        route: 'books',
+        name: 'Книги',
+        icon: <BooksIcon />,
+        id: TopLevelCategory.Books,
+    },
+    {
+        route: 'products',
+        name: 'Продукты',
+        icon: <ProductsIcon />,
+        id: TopLevelCategory.Products,
+    },
+];
